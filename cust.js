ab32a5652a345dcc9a9549af37843f4a90c9d96a
stanis94 <145879224+stanis94@users.noreply.github.com>
2023-09-26 00:23:39 +0300

    Update cust.js

--- cust.js
+++ cust.js
@@ -1,5 +1,3 @@
-<script src="https://cdnjs.cloudflare.com/ajax/libs/matter-js/0.19.0/matter.min.js"></script>
-
 function initSimulation() {
   var Engine = Matter.Engine,
     Render = Matter.Render,
